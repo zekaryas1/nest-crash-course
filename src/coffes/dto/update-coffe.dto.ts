7bac0c1b0792615e17aa9c2c19678a819b269844
zekaryas <zekaryasdinku@gmail.com>
2023-02-27 15:00:04 +0300

    id is required to update

--- src/coffes/dto/update-coffe.dto.ts
+++ src/coffes/dto/update-coffe.dto.ts
@@ -1,5 +1,10 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { IsString } from "class-validator";
 import { CreateCoffeDto } from "./create-coffe.dto";
 
-export class UpdateCoffeDto extends PartialType(CreateCoffeDto) {}
+export class UpdateCoffeDto extends PartialType(CreateCoffeDto) {
+
+    @IsString()
+    readonly id: string;
+}
   
